ed695127ca4712795f5f8621378efc5ebfa1632b
srisanthyy-crypto <srisanthyy@gmail.com>
2026-03-28 18:59:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -690,6 +690,6 @@ if __name__ == "__main__":
     init_db()
     print("=" * 50)
     print("  Study Tracker is running!")
-    print("  Open http://127.0.0.1:5000 in your browser")
+    print("  Open http://127.0.0.1:10000 in your browser")
     print("=" * 50)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
